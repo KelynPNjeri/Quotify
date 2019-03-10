2be9a2acf52ad62d816732091d4df1cfede7cab4
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-03-10 23:59:22 +0300

    Add Java code for UI.

--- app/src/main/java/com/musicalcoder/quotify/UserRegistration.java
+++ app/src/main/java/com/musicalcoder/quotify/UserRegistration.java
@@ -2,13 +2,22 @@ package com.musicalcoder.quotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UserRegistration extends AppCompatActivity {
-
+    @BindView(R.id.fullname_txt) EditText name;
+    @BindView(R.id.email_txt) EditText userEmail;
+    @BindView(R.id.password_txt) EditText userPassword;
+    @BindView(R.id.confirm_password) EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
+
+        ButterKnife.bind(this);
     }
 }
